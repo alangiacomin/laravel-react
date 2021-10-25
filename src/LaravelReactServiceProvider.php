dfe71593fdda25264a452cac235840516fca7f2d
Alan Giacomin <alan.giacomin@gmail.com>
2021-10-25 23:02:55 +0200

    publishable files for react

--- src/LaravelReactServiceProvider.php
+++ src/LaravelReactServiceProvider.php
@@ -30,6 +30,8 @@ class LaravelReactServiceProvider extends PackageServiceProvider
             __DIR__ . '/../publishable/app/Http/Controllers/HomeController.php' => app_path('Http/Controllers/HomeController.php'),
             __DIR__ . '/../publishable/routes/web.php' => base_path('routes/web.php'),
             __DIR__ . '/../publishable/resources/views/home.blade.php' => base_path('resources/views/home.blade.php'),
+            __DIR__ . '/../publishable/package.json' => base_path('package.json'),
+            __DIR__ . '/../publishable/webpack.mix.js' => base_path('webpack.mix.js'),
         ]);
     }
 }
